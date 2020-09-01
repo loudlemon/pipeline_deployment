392ded3894ef3ca1a6e5af6e2f0fa904e5008be5
loudlemon <andrey.shumanskiy@gmail.com>
2020-09-01 11:18:08 +0300

    Stage:2 -Preparing for diff testing in CI

--- packages/ml_api/tests/capture_model_predictions.py
+++ packages/ml_api/tests/capture_model_predictions.py
@@ -10,22 +10,18 @@ from api import config
 
 
 
-def capture_predictions(
-        *,
-        saved_file: str = 'test_predictions.csv'):
+def capture_predictions() -> None:
     """ Save test data predictions to .csv file"""
 
-    #saved_file = 'test_predictions.csv'
+    saved_file = 'test_predictions.csv'
     test_data = load_data(file_name='test_data.csv')
     predictions = make_prediction(input_data=test_data)
     preds_df = pd.DataFrame([predictions])
 
-#save the file into package repo
-    #preds_df.to_csv(
-        #f'{config.PACKAGE_ROOT}/{saved_file}', index=False)
+    #save the file into package repo
     preds_df.to_csv(
-        f'{config.PACKAGE_ROOT.parent}/'
-        f'log_reg/log_reg/datasets/{saved_file}', index=False)
+        f'{config.PACKAGE_ROOT}/{saved_file}', index=False)
+
 
 
 if __name__ == '__main__':
