16a509daa1e33fb6118e29d7ea320d20fd08cddf
loudlemon <andrey.shumanskiy@gmail.com>
2020-09-01 20:03:15 +0300

    Stage:2 -Differential testing in CI fix(#15)

--- packages/ml_api/tests/capture_model_predictions.py
+++ packages/ml_api/tests/capture_model_predictions.py
@@ -12,12 +12,16 @@ from api import config
 
 def capture_predictions() -> None:
     """ Save test data predictions to .csv file"""
-
+    
+    new_dict = {}
     saved_file = 'test_predictions.csv'
     test_data = load_data(file_name='test_data.csv')
     predictions = make_prediction(input_data=test_data)
-    print(predictions)
-    preds_df = pd.DataFrame([predictions])
+    array = predictions.get('predictions')[0]
+    version = predictions.get('version')
+    new_dict['prediction'] = array
+    new_dict['version'] = version
+    preds_df = pd.DataFrame([new_dict])
 
     #save the file into package repo
     preds_df.to_csv(
