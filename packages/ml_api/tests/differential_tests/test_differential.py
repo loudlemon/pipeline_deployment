18b1b2af42e7e2ab83f6322d4aadf8b6b9ec5cc3
loudlemon <andrey.shumanskiy@gmail.com>
2020-09-01 12:22:02 +0300

    Stage:2 -Differential testing in CI fix(#3)

--- packages/ml_api/tests/differential_tests/test_differential.py
+++ packages/ml_api/tests/differential_tests/test_differential.py
@@ -2,6 +2,7 @@ import math
 import pytest
 import pandas as pd
 from api import config
+from log_reg import config as model_config
 from log_reg.predict import make_prediction
 from log_reg.processing.data_management import load_data
 
@@ -17,7 +18,7 @@ def test_model_prediction_differential(
     # Given
     previous_model_df = pd.read_csv(f'{config.PACKAGE_ROOT}/{saved_file}')
     previous_model_preds = previous_model_df.predictions.values
-    test_data = load_data(file_name=config.TEST_DATA)
+    test_data = load_data(file_name=model_config.TEST_DATA)
 
     # When
     current = make_prediction(input_data=test_data)
